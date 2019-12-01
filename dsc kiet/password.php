e1204d7bfd959bbc622142aed46581b8ba250964
shikha-pal <shikhapal804@gmail.com>
2019-12-01 17:02:18 +0530

    final commit

---
+++ dsc kiet/password.php
@@ -0,0 +1,27 @@
+<?php
+
+
+if($_POST)
+{
+$host="localhost";
+$user="root";
+$pass="";
+$db="insertdb";
+$Username=$_POST["username"];
+$Password=$_POST["password"];
+$conn=mysqli_connect($host,$user,$pass,$db);
+$query="SELECT * FROM userpwd  where NAME='$Username' and PASSWORD='$Password' ";
+$result=mysqli_query($conn,$query);
+if(mysqli_num_rows($result)==1)
+{
+session_start();
+$_SESSION['insertdb']='true';
+header('location:new.html');
+
+}
+else{
+session_start();
+$_SESSION['insertdb']='true';
+header('location:login2.html');
+}
+}?>
